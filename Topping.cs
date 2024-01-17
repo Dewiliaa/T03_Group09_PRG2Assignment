41f048114a3afc8186c6b0d01586b9ae45784a5c
Dewi Lia Virnada <S10262440@connect.np.edu.sg>
2024-01-17 23:48:08 +0800

    Added Topping class.

---
+++ Topping.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T03_Group09_PRG2Assignment
+{
+    class Topping
+    {
+        //Properties
+        public string Type { get; set; }
+
+        //Constructors
+        public Topping() { }
+
+        public Topping(string type)
+        {
+            Type = type;
+        }
+
+        //ToString Method
+        public override string ToString()
+        {
+            return $"Topping: {Type}";
+        }
+    }
+}
