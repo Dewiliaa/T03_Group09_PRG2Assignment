52a37e95cc557fe829a25d7d9c402c4dddbd499f
Dewi Lia Virnada <S10262440@connect.np.edu.sg>
2024-01-18 00:16:45 +0800

    Added Cup class.

---
+++ Cup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T03_Group09_PRG2Assignment
+{
+    class Cup : IceCream
+    {
+        //Constructors
+        public Cup() : base() { }
+
+        public Cup(string option, int scoops, List<Flavour> flavours, List<Topping> toppings)
+            : base(option, scoops, flavours, toppings) { }
+
+        //Method on CalculatePrice for Cup
+        public override double CalculatePrice()
+        {
+            double basePrice = base.CalculatePrice();
+            return basePrice;
+        }
+
+        //ToString
+        public override string ToString()
+        {
+            return $"Cup - {base.ToString()}";
+        }
+    }
+}
