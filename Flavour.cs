00049ca2e660d63de2fedc5ddf08c3da8651e751
Dewi Lia Virnada <S10262440@connect.np.edu.sg>
2024-01-17 23:47:12 +0800

    Added Flavour class.

---
+++ Flavour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T03_Group09_PRG2Assignment
+{
+    class Flavour
+    {
+        //Properties
+        public string Type { get; set; }
+        public bool Premium { get; set; }
+        public int Quantity { get; set; }
+
+
+        //Constructors
+        public Flavour() { }
+
+        public Flavour(string type, bool premium, int quantity)
+        {
+            Type = type;
+            Premium = premium;
+            Quantity = quantity;
+        }
+
+        //ToString Method
+        public override string ToString()
+        {
+            return $"Flavour: {Type} - Premium: {Premium} - Quantity: {Quantity}";
+        }
+    }
+}
+
